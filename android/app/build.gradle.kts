a6ff46971732212d44a14c6af6c6a98e82fb49a6
Mingkun <1250831+Mingkun@users.noreply.github.com>
2026-09-23 12:30:05 +0800

    White document background for TXT/Word/Excel views (Android v3.2 / Win 1.1.1)

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.mingkun.pdfviewer"
         minSdk = 26
         targetSdk = 34
-        versionCode = 24
-        versionName = "3.1"
+        versionCode = 25
+        versionName = "3.2"
     }
 
     buildTypes {
